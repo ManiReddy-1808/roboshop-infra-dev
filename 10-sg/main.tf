19be70e112e3ce8bd6c2879cf83f6f34f35543f1
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-11 21:28:35 +0530

    Added NEW code

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,7 +1,8 @@
 module "sg" {
-  source = "../../terraform-aws-sg"
+  count = length(var.sg_names)
+  source = "git::https://github.com/ManiReddy-1808/terraform-aws-sg.git?ref=main"
   project = var.project
   environment = var.environment
-  sg_name = var.sg_name
+  sg_name = replace(var.sg_names[count.index], "_", "-")
   vpc_id = local.vpc_id
 }
--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -6,6 +6,19 @@ variable "environment" {
     default = "dev"
 }
 
-variable "sg_name" {
-    default = "mongodb"
+variable "sg_names" {
+  type = list
+  default = [
+    # Databases
+    "mongodb", "redis", "mysql", "rabbitmq",
+
+    # Backend
+    "catalogue", "cart", "user", "payment", "shipping",
+    "backend_alb",
+
+    # Frontend  
+    "frontend",
+    "frontend_alb",
+    "bastion"
+  ]
 }
