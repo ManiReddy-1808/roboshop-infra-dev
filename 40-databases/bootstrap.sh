5eb9c0624c23e7f4744c907e7c32a8eba801f244
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-14 15:39:20 +0530

    Added NEW code

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,3 +3,4 @@
 # This script is used to bootstrap the MongoDB server after provisioning. It will be executed by Terraform using the terraform_data resource.
 
 # Update the package index and install MongoDB
+dnf install ansible -y
