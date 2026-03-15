1a35266c9146fe6b06b89097c597410e32c55ec7
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-15 15:32:35 +0530

    Added NEW code

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -1,12 +1,11 @@
 resource "aws_route53_record" "mongodb" {
   zone_id = var.zone_id
-  name = "mongodb-${var.environment}-${var.domain_name}"
-    type = "A"
-    ttl = "1"
-    records = [aws_instance.mongodb.private_ip]
-    allow_overwrite = true
+  name    = "mongodb-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.mongodb.private_ip]
+  allow_overwrite = true
 }
-
 resource "aws_route53_record" "redis" {
   zone_id = var.zone_id
   name = "redis-${var.environment}-${var.domain_name}"
