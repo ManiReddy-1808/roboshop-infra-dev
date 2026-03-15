89c1312f4e9885fac61dca6e46fe10ebe084d014
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-15 06:26:20 +0530

    Added NEW code

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -37,19 +37,3 @@ data "aws_ssm_parameter" "user_sg_id" {
   name = "/${var.project}/${var.environment}/user_sg_id"
 }
 
-data "aws_ssm_parameter" "cart_sg_id" {
-  name = "/${var.project}/${var.environment}/cart_sg_id"
-}
-
-data "aws_ssm_parameter" "shipping_sg_id" {
-  name = "/${var.project}/${var.environment}/shipping_sg_id"
-}
-
-data "aws_ssm_parameter" "payment_sg_id" {
-  name = "/${var.project}/${var.environment}/payment_sg_id"
-}
-
-#Frontend
-data "aws_ssm_parameter" "frontend_sg_id" {
-  name = "/${var.project}/${var.environment}/frontend_sg_id"
-}
