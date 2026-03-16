58272c578181ef8642bf878d8c9936da6ff2d38b
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-03-16 12:17:16 +0530

    Added NEW code

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z02030392LMN8KQVWR9UZ"
+}
+
+variable "domain_name" {
+  default = "dawsmani.site"
+}
